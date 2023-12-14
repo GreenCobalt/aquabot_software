92bdda5863ee741ac92228f8adc485d59e046a3f
GreenCobalt <34927146+GreenCobalt@users.noreply.github.com>
2023-12-14 11:29:20 -0500

    fix

--- bot.py
+++ bot.py
@@ -145,7 +145,7 @@ class THRUST:
         else:
             return 0
         
-    def translate(value, leftMin, leftMax, rightMin, rightMax):
+    def translate(self, value, leftMin, leftMax, rightMin, rightMax):
         leftSpan = leftMax - leftMin
         rightSpan = rightMax - rightMin
         valueScaled = float(value - leftMin) / float(leftSpan)
