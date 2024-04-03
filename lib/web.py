1b04483ad382de76e5663efc6cffe1b10e274810
GreenCobalt <34927146+GreenCobalt@users.noreply.github.com>
2024-04-03 11:45:56 -0400

    ewcdf

--- lib/web.py
+++ lib/web.py
@@ -7,7 +7,7 @@ from websocket_server import WebsocketServer
 class WEB():
     def __init__(self, DEBUG, host, port):
         self.controller_values = json.loads(
-            '{"axes":{"axis_l":[0,0],"axis_r":[0,0],"trigger_l":0,"trigger_r":0},"buttons":{"button_a":false,"button_b":false,"button_x":false,"button_y":false,"button_trigger_l":false,"button_trigger_r":false,"button_select":false,"button_start":false,"button_thumb_l":false,"button_thumb_r":false}}')
+            '{"axes":{"axis_l":[128,128],"axis_r":[128,128],"trigger_l":0,"trigger_r":0},"buttons":{"button_a":false,"button_b":false,"button_x":false,"button_y":false,"button_trigger_l":false,"button_trigger_r":false,"button_select":false,"button_start":false,"button_thumb_l":false,"button_thumb_r":false}}')
 
         self.server = WebsocketServer(
             host=host, port=port, loglevel=logging.INFO)
